b27369850a671af367911eb678ee44a8ee9af30f
adit-cihuy <mrslow321@gmail.com>
2024-12-11 00:47:09 +0700

    add limit product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -59,7 +59,7 @@ const addProduct = async (req, res) => {
 // function for list product
 const listProducts = async (req, res) => {
   try {
-    const products = await productModel.find({});
+    const products = await productModel.find({}).limit(10).toArray();
     res.json({ success: true, products });
   } catch (error) {
     console.log(error);
